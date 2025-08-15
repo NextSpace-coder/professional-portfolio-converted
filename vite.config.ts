b6776894bd478ada00668ad6ad90af35fabba6b8
NextSpace-coder <nextspace.coder@example.com>
2025-08-15 12:05:23 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTagger from "vite-tagger"
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTagger from "vite-tagger";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(() => {
@@ -9,19 +9,16 @@ export default defineConfig(() => {
     server: {
       host: "::",
       port: 8080,
-      allowedHosts: true,
+      allowedHosts: true as true,
     },
-    plugins: [
-      react(),
-      viteTagger({ prefixName: "wb" }),
-    ],
+    plugins: [react(), viteTagger({ prefixName: "wb" })],
     resolve: {
       alias: {
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@utils': path.resolve(__dirname, 'src/utils'),
-        '@pages': path.resolve(__dirname, 'src/pages'),
-        '@layout': path.resolve(__dirname, 'src/layout')
-      }
-    }
+        "@components": path.resolve(__dirname, "src/components"),
+        "@utils": path.resolve(__dirname, "src/utils"),
+        "@pages": path.resolve(__dirname, "src/pages"),
+        "@layout": path.resolve(__dirname, "src/layout"),
+      },
+    },
   };
 });
